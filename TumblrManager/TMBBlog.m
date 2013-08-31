bc5098645ee43a32b00f6409177547244eb085ea
Luc Le Quiniat <luc.lequiniat@me.com>
2013-08-31 15:03:56 +1000

    Revert "Broken pods stuff"
    This reverts commit 5bc688fc5b463587a056cd8681949de14d967cef.

--- TumblrManager/TMBBlog.m
+++ TumblrManager/TMBBlog.m
@@ -7,25 +7,13 @@
 //
 
 #import "TMBBlog.h"
-#import "TMAPIClient.h"
 
 @implementation TMBBlog
 
 - (id) initWithDetails
 {
     if ((self = [super init])) {
-        
-        // Authenticate via API Key
-        [TMAPIClient sharedInstance].OAuthConsumerKey = @"1kNHEcklCA6h5i7xG4HDsJA4QVcYN9tNsUgmxCqzX2bxsK7VjX";
-        
-        // Make the request
-        [[TMAPIClient sharedInstance] blogAvatar:@"fairlyhuman.tumblr.com"
-                                            size:64
-                                        callback:^ (id result, NSError *error) {
-                                            // ...
-                                        }];
-        
-        
+                
         _blogTitle = @"Blog Title";
         _blogAvatar = [UIImage imageNamed:@"QuestionMark.png"];
         _blogDescription = @"Blog description goes here";
